b4785880593f7822af84306031f03c3720052b85
katwold <61921125+katwold@users.noreply.github.com>
2020-05-24 20:22:15 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+// question 1 
+var firstVar;
+// question 2
+var name = "Katarina";
+// question 3
+var integer = 29;
+// question 4
+var division = 20/5;
+// question 5
+typeof("frog");
+// question 6
+var orderHasShipped = true;
+// question 7
+var orderHasShipped = true;
+
+if(orderHasShipped === true) {
+    console.log("true");
+}
+else {
+    console.log("false");
+}
+// question 8
+for(var count = 0; count <= 9; count++) {
+    console.log(count);
+}
